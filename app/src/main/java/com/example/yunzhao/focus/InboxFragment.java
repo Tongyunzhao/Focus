b633a576f285314a6f2e4ba24ffbb6bfe7d5426c
Tongyunzhao <314342613@qq.com>
2018-03-09 10:54:57 +0800

    完成APP抽屉、顶部tab导航等基本框架

---
+++ app/src/main/java/com/example/yunzhao/focus/InboxFragment.java
@@ -0,0 +1,27 @@
+package com.example.yunzhao.focus;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yunzhao on 2018/3/9.
+ */
+
+public class InboxFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.inbox_layout, container,false);
+        return view;
+    }
+}
