227aabfc4231b50f5a5345b91b73be6e7cea91a1
Tongyunzhao <314342613@qq.com>
2018-03-19 00:39:46 +0800

    用科大讯飞SDK完成语音输入模式

--- app/src/main/java/com/example/yunzhao/focus/MainActivity.java
+++ app/src/main/java/com/example/yunzhao/focus/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.yunzhao.focus;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -28,6 +40,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
@@ -40,12 +53,29 @@ import com.baoyz.swipemenulistview.SwipeMenuItem;
 import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.example.yunzhao.focus.MyListViewAdapter.onItemDoneListener;
 import com.example.yunzhao.focus.util.DimenUtil;
+import com.example.yunzhao.focus.util.JsonParser;
 import com.example.yunzhao.focus.util.StatusBarUtil;
 import com.example.yunzhao.focus.widget.MyEditDialog;
 import com.example.yunzhao.focus.widget.MyListView;
 import com.example.yunzhao.focus.widget.MyListView2;
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+import com.iflytek.sunflower.FlowerCollector;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -54,7 +84,7 @@ import static com.example.yunzhao.focus.util.DimenUtil.dp2px;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, View.OnTouchListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnTouchListener, SensorEventListener {
 
     private MyListView todo_listview;
     private MyListView inbox_listview;
@@ -77,7 +107,8 @@ public class MainActivity extends AppCompatActivity
 
     // 音效
     private SoundPool soundPool;  // 声明一个SoundPool
-    private int soundID;  // 创建某个声音对应的音频ID
+    private int soundID;  // 完成任务的音效
+    private int soundID_;  // 开始语音识别的音效
 
     // 今日待办、收件箱、已完成 3个清单的适配器
     private MyListViewAdapter adapter0;  // 今日待办的适配器
@@ -86,6 +117,24 @@ public class MainActivity extends AppCompatActivity
 
     private int operation_position;  // 记录当前要进行操作的item位置
 
+    // 摇一摇操作
+    private static final int SENSOR_SHAKE = 40;
+    private boolean isShake = false;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometerSensor;
+
+    // 科大讯飞语音识别
+    private SpeechRecognizer mIat = null;  // 语音听写对象
+    private RecognizerDialog mIatDialog = null;  // 语音听写UI
+    private String mEngineType = SpeechConstant.TYPE_CLOUD;  // 引擎类型
+    // 用HashMap存储听写结果
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
+    int ret = 0; // 函数调用返回值
+    private BottomSheetDialog yuyinDialog;
+
+
+    // 权限
+    private static final int MY_PERMISSION_REQUEST_CODE = 10000;
 
     private Handler handler = new Handler() {
         @Override
@@ -136,6 +185,20 @@ public class MainActivity extends AppCompatActivity
             initSound();
         }
 
+        // 初始化科大讯飞语音识别
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=5aac8016");
+        initYuYin();
+    }
+
+    private void initYuYin() {
+        mIat = null;
+        mIatDialog = null;
+
+        // 使用SpeechRecognizer对象，可根据回调消息自定义界面
+        mIat = SpeechRecognizer.createRecognizer(this, mInitListener);
+        // 初始化听写Dialog，如果只使用有UI听写功能，无需创建SpeechRecognizer
+        // 使用UI听写功能，请根据sdk文件目录下的notice.txt,放置布局文件和图片资源
+        mIatDialog = new RecognizerDialog(this, mInitListener);
     }
 
     private void setToolbar() {
@@ -160,6 +223,7 @@ public class MainActivity extends AppCompatActivity
     private void initSound() {
         soundPool = new SoundPool.Builder().build();
         soundID = soundPool.load(this, R.raw.done, 1);
+        soundID_ = soundPool.load(this, R.raw.ding, 1);
     }
 
     private void initDoneTask() {
@@ -369,6 +433,30 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
+    @Override
+    protected void onResume() {
+        //获取 SensorManager 负责管理传感器
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (mSensorManager != null) {
+            //获取加速度传感器
+            mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (mAccelerometerSensor != null) {
+                mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(this);
+        }
+        super.onPause();
+    }
+
+
     private void setAddTask() {
         Button btn_addtodaytask = findViewById(R.id.btn_addtodaytask);
         Button btn_addinboxtask = findViewById(R.id.btn_addinboxtask);
@@ -458,7 +546,6 @@ public class MainActivity extends AppCompatActivity
         adapter1.notifyDataSetChanged();
         adapter2.notifyDataSetChanged();
     }
-
 
 
     // “今日待办”置顶按钮点击监听事件
@@ -649,4 +736,293 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         }
     };
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int type = event.sensor.getType();
+
+        if (type == Sensor.TYPE_ACCELEROMETER) {
+            //获取三个方向值
+            float[] values = event.values;
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            if ((Math.abs(x) > SENSOR_SHAKE || Math.abs(y) > SENSOR_SHAKE || Math.abs(z) > SENSOR_SHAKE) && !isShake && (scrollView.getVisibility() == View.VISIBLE)) {
+                isShake = true;
+                soundPool.play(soundID_, 0.5f, 0.5f, 0, 0, 1);
+
+                Log.d("yuyin", "进入onSensorChanger");
+
+                /**
+                 * 检查是否有「语音转文字」所需的相应权限
+                 */
+                final boolean isAllGranted = checkPermissionAllGranted(
+                        new String[]{
+                                Manifest.permission.RECORD_AUDIO
+                        }
+                );
+
+                if (isAllGranted) {
+                    xunfei();
+                } else {
+                    // 一次请求多个权限, 如果其他有权限是已经授予的将会自动忽略掉
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{
+                                    Manifest.permission.RECORD_AUDIO
+                            },
+                            MY_PERMISSION_REQUEST_CODE
+                    );
+                    isShake = false;
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+
+    /**
+     * 参数设置
+     *
+     * @return
+     */
+    public void setParam() {
+        // 清空参数
+        mIat.setParameter(SpeechConstant.PARAMS, null);
+        // 设置听写引擎
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+        // 设置返回结果格式
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+        // 设置语言
+        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        // 设置语言区域
+        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+
+        //此处用于设置dialog中不显示错误码信息
+        //mIat.setParameter("view_tips_plain","false");
+
+        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+        mIat.setParameter(SpeechConstant.VAD_BOS, "5000");
+
+        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+        mIat.setParameter(SpeechConstant.VAD_EOS, "1500");
+
+        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT, "0");
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
+        mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
+    }
+
+    /**
+     * 听写UI监听器
+     */
+    private RecognizerDialogListener mRecognizerDialogListener = new RecognizerDialogListener() {
+        public void onResult(RecognizerResult results, boolean isLast) {
+            if (!isLast)
+                addTask(results);
+        }
+
+        /**
+         * 识别回调错误.
+         */
+        public void onError(SpeechError error) {
+            mIatDialog.dismiss();
+            isShake = false;
+            Snackbar.make(scrollView, "你好像没有说话哦", Snackbar.LENGTH_SHORT).show();
+        }
+
+    };
+
+
+    private void addTask(RecognizerResult results) {
+        String text = JsonParser.parseIatResult(results.getResultString());
+
+        String sn = null;
+        // 读取json结果中的sn字段
+        try {
+            JSONObject resultJson = new JSONObject(results.getResultString());
+            sn = resultJson.optString("sn");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mIatResults.put(sn, text);
+
+        StringBuffer resultBuffer = new StringBuffer();
+        for (String key : mIatResults.keySet()) {
+            resultBuffer.append(mIatResults.get(key));
+        }
+
+        String newTaskName = resultBuffer.toString();
+        TaskItem newTask = new TaskItem(false, newTaskName, false);
+        inboxTaskItems.add(0, newTask);
+        refreshListView();
+
+        isShake = false;
+
+        Snackbar.make(scrollView, "成功添加一条任务到「收件箱」", Snackbar.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 初始化监听器。
+     */
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            Log.d("yuyin", "SpeechRecognizer init() code = " + code);
+            if (code != ErrorCode.SUCCESS) {
+                Toast.makeText(MainActivity.this, "初始化失败，错误码：" + code, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+
+    /**
+     * 检查是否拥有指定的所有权限
+     */
+    private boolean checkPermissionAllGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 科大讯飞语音转文字
+    private void xunfei() {
+
+        if (mIat == null) {
+            // 创建单例失败，与 21001 错误为同样原因，参考 http://bbs.xfyun.cn/forum.php?mod=viewthread&tid=9688
+            Toast.makeText(this, "创建对象失败，请确认 libmsc.so 放置正确，且有调用 createUtility 进行初始化", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // 移动数据分析，收集开始听写事件
+        FlowerCollector.onEvent(MainActivity.this, "iat_recognize");
+
+        mIatResults.clear();
+
+        // 设置参数
+        setParam();
+
+        // 显示听写对话框
+//        mIatDialog.setListener(mRecognizerDialogListener);
+//        mIatDialog.show();
+
+
+        // 不显示听写对话框
+        ret = mIat.startListening(mRecognizerListener);
+        if (ret != ErrorCode.SUCCESS) {
+            isShake = false;
+            //Toast.makeText(this, "听写失败,错误码：" + ret, Toast.LENGTH_SHORT).show();
+            Snackbar.make(scrollView, "好像出了点小问题", Snackbar.LENGTH_SHORT).show();
+        } else {
+            showYuYinDialog();
+            //Toast.makeText(this, "请开始说话", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /**
+     * 申请权限结果返回处理
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            boolean isAllGranted = true;
+
+            // 判断是否所有的权限都已经授予了
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            if (isAllGranted) {
+                xunfei();
+            }
+        }
+    }
+
+
+    /**
+     * 听写监听器。
+     */
+    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+        @Override
+        public void onBeginOfSpeech() {
+            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
+            //showTip("开始说话");
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+            // Tips：
+            // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
+            //Toast.makeText(MainActivity.this, error.getPlainDescription(true), Toast.LENGTH_SHORT).show();
+            Snackbar.make(scrollView, "你好像没有说话哦", Snackbar.LENGTH_SHORT).show();
+            isShake = false;
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
+            //Toast.makeText(MainActivity.this, "结束说话", Toast.LENGTH_SHORT).show();
+            yuyinDialog.dismiss();
+        }
+
+        @Override
+        public void onResult(RecognizerResult results, boolean isLast) {
+            if (!isLast)
+                addTask(results);
+        }
+
+        @Override
+        public void onVolumeChanged(int volume, byte[] data) {
+            //showTip("当前正在说话，音量大小：" + volume);
+            //Log.d(TAG, "返回音频数据："+data.length);
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+            // 若使用本地能力，会话id为null
+            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+            //		Log.d(TAG, "session id =" + sid);
+            //	}
+        }
+    };
+
+
+    private void showYuYinDialog() {
+        yuyinDialog = new BottomSheetDialog(MainActivity.this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_yuyin, null);
+        yuyinDialog.setContentView(dialogView);
+
+        ImageView iv = dialogView.findViewById(R.id.iv_microphone);
+        iv.setBackgroundResource(R.drawable.microphone_anim);
+        AnimationDrawable ad = (AnimationDrawable) iv.getBackground();
+        ad.start();
+
+        yuyinDialog.show();
+    }
+
 }
+
