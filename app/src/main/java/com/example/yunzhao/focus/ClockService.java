a443b2e2fa71c4be5c9b0e199c648133147be98c
Tongyunzhao <314342613@qq.com>
2018-05-29 19:04:48 +0800

    修复黑屏暂停计时的BUG，新增计时结束的震动提醒

--- app/src/main/java/com/example/yunzhao/focus/ClockService.java
+++ app/src/main/java/com/example/yunzhao/focus/ClockService.java
@@ -4,12 +4,16 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -35,6 +39,9 @@ public class ClockService extends Service {
     private SoundPool soundPool;  // 声明一个SoundPool
     private int soundID;  // 创建某个声音对应的音频ID
 
+    // 震动
+    private Vibrator vibrator;
+
     private String taskname;
     private long taskID;
 
@@ -48,6 +55,8 @@ public class ClockService extends Service {
     // 数据存储
     private DatabaseHelper db;
 
+    private PowerManager.WakeLock wakeLock = null;
+
 
     @Override
     public void onCreate() {
@@ -58,8 +67,16 @@ public class ClockService extends Service {
             initSound();
         }
 
+        // 初始化震动
+        vibrator = (Vibrator) getApplication().getSystemService(Service.VIBRATOR_SERVICE);
+
         initNotification();  // 初始化通知
         db = new DatabaseHelper(this);  // 初始化数据库
+
+        // 保证 Service 在黑屏后保持运行
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, ClockService.class.getName());
+        wakeLock.acquire();
     }
 
     @Override
@@ -88,6 +105,7 @@ public class ClockService extends Service {
                     if (progress == POMODORO_LENGTH) {
                         soundPool.play(soundID, 0.5f, 0.5f, 100, 0, 1);  // 播放音效
                         notificationManager.notify(0, notification);
+                        vibrator.vibrate(new long[]{0, 1200, 300, 1200, 300, 1200}, -1);
                         // 将操作写入OpRecord表
                         db.createOpRecord(1, taskID, new Date().getTime());
                     }
@@ -95,6 +113,8 @@ public class ClockService extends Service {
                     //发送广播
                     intent.putExtra("progress", progress);
                     sendBroadcast(intent);
+
+                    Log.e("Timer", progress+"");
                 }
             };
             timer.schedule(task, 1000, 1000);
@@ -119,8 +139,7 @@ public class ClockService extends Service {
 
         notification = new Notification.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("完成一次专注").setContentText("休息一下，一会继续吧")
-                .setContentIntent(pendingIntent).setAutoCancel(true)
-                .setDefaults(Notification.DEFAULT_VIBRATE).build();
+                .setContentIntent(pendingIntent).setAutoCancel(true).build();
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 
@@ -128,6 +147,10 @@ public class ClockService extends Service {
     public void onDestroy() {
         super.onDestroy();
         stopTimer();
+        if (wakeLock != null) {
+            wakeLock.release();
+            wakeLock = null;
+        }
         db.closeDB();
     }
 }
